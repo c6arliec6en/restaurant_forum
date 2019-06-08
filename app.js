b568424b877af40ddec72373a37a519e461edd26
Charlie <chen_610146@hotmail.com>
2019-06-08 14:31:04 +0800

    Add flash messages

--- app.js
+++ app.js
@@ -4,12 +4,25 @@ const port = 3000
 const exphbs = require('express-handlebars')
 const db = require('./models')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+const flash = require('connect-flash')
+
 
 app.set('view engine', 'handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'Hello',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
 
-
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 
 app.listen(port, () => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -7,13 +7,28 @@ const userControllers = {
   },
 
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
-      return res.redirect('/signin')
-    })
+    if (req.body.password !== req.body.passwordCheck) {
+      req.flash('error_messages', '兩次密碼輸入不同')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          req.flash('error_messages', '此信箱已註冊')
+          return res.redirect('/signup')
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
+          }).then(user => {
+            req.flash('ssuccess_messages', '註冊成功')
+            return res.redirect('/signin')
+          })
+        }
+
+      })
+    }
+
   }
 }
 
