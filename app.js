dbe004dc7458ca8288edf1c4ee5518030e0ea088
Charlie <chen_610146@hotmail.com>
2019-06-08 12:11:17 +0800

    connect to database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
+const db = require('./models')
 
 app.set('view engine', 'handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -11,5 +12,9 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 
 
 app.listen(port, () => {
+  db.sequelize.sync()
   console.log("Express running")
 })
+
+
+require('./routes')(app)
