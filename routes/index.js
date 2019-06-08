88b8ebbd166d6b757e7f35bb8c590e0d63c4a547
Charlie <chen_610146@hotmail.com>
2019-06-08 17:48:49 +0800

    Authenticate normal user and admin

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,26 @@ const adminController = require('../controllers/adminController')
 const userController = require('../controllers/userController')
 
 module.exports = (app, passport) => {
-  app.get('/', (req, res) => res.redirect('/restaurants'))
-  app.get('/restaurants', restController.getRestaurants)
 
-  app.get('/admin', (req, res) => res.redirect('admin/restaurants'))
-  app.get('/admin/restaurants', adminController.getRestaurants)
+  const authenticate = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    } return res.redirect('/signin')
+  }
+
+  const isAdminAuthenticate = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.isAdmin) { return next() }
+      return res.redirect('/')
+    }
+    return res.redirect('/signin')
+  }
+
+  app.get('/', authenticate, (req, res) => res.redirect('/restaurants'))
+  app.get('/restaurants', authenticate, restController.getRestaurants)
+
+  app.get('/admin', isAdminAuthenticate, (req, res) => res.redirect('admin/restaurants'))
+  app.get('/admin/restaurants', isAdminAuthenticate, adminController.getRestaurants)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
