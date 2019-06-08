705df65aa6a2f975bcbea12317855e6dd5c0c9b2
Charlie <chen_610146@hotmail.com>
2019-06-08 14:11:07 +0800

    Add register function

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
 const db = require('./models')
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
